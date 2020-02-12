a1766cf10345e3ca8c906cd44593feba3fabdf2e
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2020-02-12 11:44:51 +0100

    Created basic scene
    We have created a 10x10 basic tilemap scene and created a sample cell that changes its albedo if you cannot pass through it.

---
+++ Project Escape/Assets/Scripts/CellBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool canPass;
+    private Renderer thisRend;
+
+    void Start()
+    {
+        thisRend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPass)
+        {
+            thisRend.material.SetColor("_Color", Color.white);
+        }
+        else
+        {
+            thisRend.material.SetColor("_Color", Color.red);
+        }
+    }
+}
