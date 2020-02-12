3e4408eadee8f72d4a3b22283285b51481ee761b
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2020-02-12 12:52:53 +0100

    Created a basic map generation
    Created a basic map generation using a integer matrix and changing the cell behaviour depending on the values of the matrix.

---
+++ Project Escape/Assets/Scripts/CellMap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMap : MonoBehaviour
+{
+    private int[,] coordinates;
+    public CellBehaviour cell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+            0 -> Empty cell
+            1 -> Cannot pass cell
+            2 -> Can destroy cell
+            3 -> Start
+            4 -> End
+        */
+
+        coordinates = new int[10, 10] {
+            {0, 0, 0, 2, 0, 0, 0, 1, 0, 4},
+            {0, 1, 1, 1, 0, 1, 0, 1, 2, 1},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 1, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 1, 1, 1, 1, 0, 2},
+            {0, 0, 0, 0, 1, 0, 1, 1, 0, 0},
+            {0, 3, 0, 0, 1, 0, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        for (int i = 0; i < coordinates.GetLength(0); i++)
+        {
+            for(int j = 0; j < coordinates.GetLength(1); j++)
+            {
+                Vector3 position = new Vector3(i - 4.5f, -0.5f, j - 4.5f);
+                Quaternion rotation = new Quaternion();
+                if (coordinates[i, j] != 0) cell.canPass = false;
+                else cell.canPass = true;
+                Instantiate(cell, position, rotation);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
